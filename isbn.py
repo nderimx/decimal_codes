f9efa1b7adde0a0ef86c9593f24b3610374a1a22
nderimx <nderimx@gmail.com>
2021-03-30 15:57:43 +0200

    isbn13 added

--- isbn.py
+++ isbn.py
@@ -17,21 +17,43 @@ def calc_isbn_suffix(n):
         suffix = "X"
     return suffix
 
+def calc_isbn13_suffix(n):
+    code_sum = 0
+    for i, digit in enumerate(n):
+        if i%2==0:
+            code_sum+=int(digit)
+        else:
+            code_sum+=int(digit)*3
+    r = 10 - code_sum % 10
+    if r < 10:
+        suffix = str(r)
+    elif r == 10:
+        suffix = "0"
+    return suffix
+
+
 def isbn(n):
-    if len(n)==9:
+    if len(n)==9 or len(n)==12:
         try:
             int(n)
         except:
             return "error: input is not an integer."+"\tDon't use hyphens between digits"
-        return n+calc_isbn_suffix(n)
-    elif len(n)==10:
+        if len(n) ==9:
+            return n+calc_isbn_suffix(n)
+        else:
+            return n+calc_isbn13_suffix(n)
+    elif len(n)==10 or len(n)==13:
         try:
             int(n[:len(n)-1])
         except:
             return "error: input w/o parity suffix is not an integer"+"\tDon't use hyphens between digits"
         parity = n[-1]
-        return calc_isbn_suffix(n[:len(n)-1]) == parity
+        if len(n)==10:
+            return calc_isbn_suffix(n[:len(n)-1]) == parity
+        else:
+            return calc_isbn13_suffix(n[:len(n)-1]) == parity
+
     else:
-        return "error: input is not 9 or 10"+"\tDon't use hyphens between digits"
+        return "error: input is not 9 or 10, 12 or 13"+"\tDon't use hyphens between digits"
 
 print(isbn(sys.argv[1]))
